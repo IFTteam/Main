08848b8e4bedd54a154a3c6c374f6dfe1c0e7ab8
james <jameswang962050@gmail.com>
2022-09-07 08:48:20 -0700

    sa

--- src/main/java/springredis/demo/tasks/TaskExecutor.java
+++ src/main/java/springredis/demo/tasks/TaskExecutor.java
@@ -1,27 +1,22 @@
 package springredis.demo.tasks;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.client.RestTemplate;
 import springredis.demo.entity.CoreModuleTask;
 import springredis.demo.entity.Node;
 import springredis.demo.entity.activeEntity.ActiveAudience;
 import springredis.demo.entity.activeEntity.ActiveNode;
-//import springredis.demo.entity.otherModuleEntity.TimeTask;
+import springredis.demo.entity.base.BaseTaskEntity;
 import springredis.demo.repository.NodeRepository;
 import springredis.demo.repository.activeRepository.ActiveAudienceRepository;
 import springredis.demo.repository.activeRepository.ActiveJourneyRepository;
 import springredis.demo.repository.activeRepository.ActiveNodeRepository;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 
 public class TaskExecutor implements Runnable {
-
-    @Autowired
-    RestTemplate restTemplate;
-
     private CoreModuleTask coreModuleTask;
 
     private ActiveAudienceRepository activeAudienceRepository;
@@ -31,6 +26,19 @@ public class TaskExecutor implements Runnable {
     private ActiveJourneyRepository activeJourneyRepository;
 
     private NodeRepository nodeRepository;
+    private RestTemplate restTemplate = new RestTemplate();
+    private HashMap<String,String> urlDict = new HashMap<String,String>(){
+        {
+            put("TimeDelay","http://localhost:3000");
+            put("APITrigger","http://localhost:3001");
+            put("End","http://localhost:3002");
+            put("TimeTrigger","http://localhost:3003");
+            put("SendEmail","http://localhost:3004");
+
+        }
+
+    };
+
     public TaskExecutor(CoreModuleTask coreModuleTask) {
         this.coreModuleTask = coreModuleTask;
     }
@@ -61,7 +69,7 @@ public class TaskExecutor implements Runnable {
 
         //call API
         Optional<Node> nextNode = nodeRepository.findById(coreModuleTask.getTargetNodeId());
-        if (nextNode.isPresent()){
+        if (nextNode.isPresent() && !Objects.equals( nextNode.get().getName(),"End")){
             callModule(nextNode.get());
         }else{
             System.out.println("target node not found");
@@ -73,29 +81,22 @@ public class TaskExecutor implements Runnable {
 
     }
 
-    //call different apis at this point
     public void callModule(Node node) {
         String nodeType = node.getType();
         String nodeName = node.getName();
-        String url = new String();
-
-        if(Objects.equals(nodeType, "TimeDelay")){
-//            TimeTask timeTask = new TimeTask();
-
-        } else if (Objects.equals(nodeType, "If/Else")) {
-
-        } else if (Objects.equals(nodeType, "API Trigger")) {
-            HttpEntity<CoreModuleTask> call = new HttpEntity(coreModuleTask);
-            //replace with server name
-            url = "localhost:8080/API_trigger";
-            CoreModuleTask response = restTemplate.exchange(url,HttpMethod.POST,call,CoreModuleTask.class).getBody();
-        } else if (Objects.equals(nodeType, "SendEmail")) {
-
-        } else if (Objects.equals(nodeType, "End")) {
-
-        } else if (Objects.equals(nodeType, "Time Trigger")) {
-            
+        BaseTaskEntity baseTaskEntity = coreModuleTask;
+//        baseTaskEntity.setJourneyId(coreModuleTask.getJourneyId());
+//        baseTaskEntity.setNodeId(coreModuleTask.getNodeId());
+//        baseTaskEntity.setUserId(coreModuleTask.getUserId());
+        // switch the previous target node to new source node
+        baseTaskEntity.setSourceNodeId(coreModuleTask.getTargetNodeId());
+        // get new target node
+        if ((node.getNexts()).size()>0){
+            baseTaskEntity.setTargetNodeId(activeNodeRepository.findByDBNodeId((node.getNexts()).get(0)).getId());
         }
+        //Call module accordingly
+        restTemplate.postForObject(urlDict.get(nodeType), baseTaskEntity, String.class);
+
 
 
     }
@@ -114,7 +115,6 @@ public class TaskExecutor implements Runnable {
 
     }
 
-    //
     private void moveUser(CoreModuleTask coreModuleTask) {
         Optional<ActiveAudience> activeAudience = activeAudienceRepository.findById(coreModuleTask.getActiveAudienceId());
         if (activeAudience.isPresent()){
@@ -125,7 +125,6 @@ public class TaskExecutor implements Runnable {
 
     }
 
-    //checks whether active audience exist in
     private Boolean findAudience(Long id) {
         Optional<ActiveAudience> activeAudience = activeAudienceRepository.findById(id);
         return activeAudience.isPresent();
