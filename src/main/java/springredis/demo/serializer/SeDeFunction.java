50dbfa9121766aa9a8901485eb83b09c624cd269
Jerry Lin <a23314638@gmail.com>
2022-10-07 13:58:58 -0700

    Separate LocalDateTime serializer and deserializer from original

--- src/main/java/springredis/demo/serializer/SeDeFunction.java
+++ src/main/java/springredis/demo/serializer/SeDeFunction.java
@@ -3,13 +3,10 @@ package springredis.demo.serializer;
 import com.google.gson.*;
 import springredis.demo.entity.Node;
 
-import java.lang.reflect.Type;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
 
 public class SeDeFunction {
     //传进来的是node list，把一个一个node拿出来序列化，然后加入String，返回String
@@ -43,19 +40,5 @@ public class SeDeFunction {
         return nodeArray;
     }
 }
-class LocalDateTimeSerializer implements JsonSerializer<LocalDateTime> {
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d::MMM::uuuu HH::mm::ss");
-    @Override
-    public JsonElement serialize(LocalDateTime localDateTime, Type srcType, JsonSerializationContext context) {
-        return new JsonPrimitive(formatter.format(localDateTime));
-    }
-}
 
-class LocalDateTimeDeserializer implements JsonDeserializer < LocalDateTime > {
-    @Override
-    public LocalDateTime deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
-            throws JsonParseException {
-        return LocalDateTime.parse(json.getAsString(),
-                DateTimeFormatter.ofPattern("d::MMM::uuuu HH::mm::ss").withLocale(Locale.ENGLISH));
-    }
-}
+
