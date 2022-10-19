d6ad92a48bd181b5a55f4a316bb20f661e78d402
james <jameswang962050@gmail.com>
2022-10-19 06:11:59 -0700

    sa

--- src/main/java/springredis/demo/tasks/TaskCoordinator.java
+++ src/main/java/springredis/demo/tasks/TaskCoordinator.java
@@ -7,6 +7,7 @@ import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.core.RedisConnectionUtils;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
+import springredis.demo.entity.CoreModuleTask;
 import springredis.demo.repository.TimeDelayRepository;
 import springredis.demo.structures.OutAPICaller;
 import springredis.demo.structures.SimulateHeapKeeper;
@@ -17,11 +18,13 @@ import java.util.concurrent.Executors;
 
 @Component
 @Slf4j
-public class TaskCoordinator implements DisposableBean {
+public class TaskCoordinator implements DisposableBean,Runnable {
 
     private volatile boolean someCondition = true;
 
     private final String taskQueueKey = "CoretaskQueue";
+
+    private RedisTemplate m_redisTemplate;
 
 
 
@@ -35,12 +38,34 @@ public class TaskCoordinator implements DisposableBean {
         SimulateHeapKeeper simulateHeapKeeper = new SimulateHeapKeeper(redisTemplate);
         OutAPICaller outAPICaller = new OutAPICaller(timeDelayRepository, redisTemplate);
         SimulateNewEvent simulateNewEvent = new SimulateNewEvent(timeDelayRepository, redisTemplate);
+        this.m_redisTemplate = redisTemplate;
         new Thread(simulateNewEvent).start();
         new Thread(simulateHeapKeeper).start();
         new Thread(outAPICaller).start();
+        new Thread(this).start();                   //since task coordinator itself is also a runnable,its run function is also started when it is constructed
     }
 
 
+    @Override
+    public void run() {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);//初始化线程池
+
+
+        while (someCondition) {
+            // 查Redis
+            while (m_redisTemplate.opsForList().size(taskQueueKey)>0){
+                CoreModuleTask coreModuleTask = (CoreModuleTask) m_redisTemplate.opsForList().rightPop(taskQueueKey);
+                TaskExecutor taskExecutor = new TaskExecutor(coreModuleTask);
+                executorService.execute(taskExecutor);                //this is equivalent to using taskExecutor.run()
+
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
     @Override
     public void destroy() throws Exception {
