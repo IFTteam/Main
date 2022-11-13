d8efa9adab0567eb6f81809c598209580ab9b121
Kira Wang <ikirawang@gmail.com>
2022-11-14 00:12:46 +0800

    移动action_send

---
+++ src/main/java/springredis/demo/controller/ActionSendController.java
@@ -0,0 +1,141 @@
+package springredis.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+import springredis.demo.entity.Transmission;
+import springredis.demo.entity.base.BaseTaskEntity;
+import springredis.demo.entity.request.ScheduledTransmissionRequest;
+import springredis.demo.entity.request.TransmissionRequest;
+import springredis.demo.entity.response.Response;
+import springredis.demo.entity.response.SparkPostResponse;
+import springredis.demo.repository.AudienceRepository;
+import springredis.demo.repository.JourneyRepository;
+import springredis.demo.repository.TransmissionRepository;
+import springredis.demo.repository.UserRepository;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@Slf4j // logger
+@RestController
+@RequestMapping(path = "/actionSend")
+@CrossOrigin(origins="*")
+public class ActionSendController {
+    private final TransmissionRepository transmissionRepository;
+    private final UserRepository userRepository;
+    private final AudienceRepository audienceRepository;
+    private final JourneyRepository journeyRepository;
+    private final WebClient webClient;
+    private final RestTemplate restTemplate = new RestTemplate();
+
+
+
+    @Autowired
+    public ActionSendController(TransmissionRepository transmissionRepository,
+                                UserRepository userRepository,
+                                AudienceRepository audienceRepository,
+                                JourneyRepository journeyRepository,
+                                WebClient webClient){
+        this.transmissionRepository = transmissionRepository;
+        this.userRepository = userRepository;
+        this.audienceRepository = audienceRepository;
+        this.journeyRepository = journeyRepository;
+        this.webClient = webClient;
+    }
+
+
+    @RequestMapping(value={"/createTransmission"}, method = POST)
+    @ResponseBody
+    public ResponseEntity<Response> createTransmission(@RequestBody TransmissionRequest transmissionRequest){
+        Optional<SparkPostResponse> sparkPostResponse = webClient.post()
+                                                                 .uri("/api/v1/transmissions?num_rcpt_errors=3")
+                                                                 .header("Content-Type", "application/json")
+                                                                 .header("Accept", "application/json")
+                                                                 .accept(MediaType.APPLICATION_JSON)
+                                                                 .body(Mono.just(transmissionRequest), TransmissionRequest.class)
+                                                                 .retrieve()
+                                                                 .bodyToMono(SparkPostResponse.class)
+                                                                 .blockOptional();
+
+        if(!sparkPostResponse.isPresent()) return ResponseEntity
+                .status(HttpStatus.FAILED_DEPENDENCY).body(new Response());
+
+        //record keeping
+        Transmission transmission = new Transmission();
+        log.info("transmission id is" + sparkPostResponse.get().getSparkPostResults().getTransmissionId());
+        transmission.setId(sparkPostResponse.get().getSparkPostResults().getTransmissionId());
+        transmission.setAudience_email(transmissionRequest.getAddressList().get(0).getAddress());
+        transmission.setAudience(audienceRepository.getReferenceById(transmissionRequest.getAudienceId()));
+        transmission.setUser(userRepository.getReferenceById(transmissionRequest.getUserId()));
+        transmission.setCreatedAt(LocalDateTime.now());
+        transmission.setCreatedBy("" + transmissionRequest.getUserId());
+        transmission.setJourney(journeyRepository.findById(transmissionRequest.getJourneyId()).get());
+        transmissionRepository.save(transmission);
+
+        Response response = new Response();
+        response.setStatusCode(200);
+        response.setMsg("Transmission successfully created");
+
+        //return task to core module
+        BaseTaskEntity coreModuleTask = new BaseTaskEntity();
+        restTemplate.postForObject("http://localhost:8081/ReturnTask", coreModuleTask, String.class);
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @RequestMapping(value={"/createScheduledTransmission"}, method = POST)
+    @ResponseBody
+    public ResponseEntity<Response> createScheduleTransmission(
+                                @RequestBody ScheduledTransmissionRequest scheduledTransmissionRequest){
+        Optional<SparkPostResponse> sparkPostResponse = webClient.post()
+                .uri("/api/v1/transmissions?num_rcpt_errors=3")
+//                .header("Content-Type", "application/json")
+//                .header("Accept", "application/json")
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Mono.just(scheduledTransmissionRequest), ScheduledTransmissionRequest.class)
+                .retrieve()
+                .bodyToMono(SparkPostResponse.class)
+                .blockOptional();
+
+        if(!sparkPostResponse.isPresent()) return ResponseEntity
+                .status(HttpStatus.FAILED_DEPENDENCY).body(new Response());
+
+        //record keeping
+        Transmission transmission = new Transmission();
+        log.info("transmission id is" + sparkPostResponse.get().getSparkPostResults().getTransmissionId());
+        transmission.setId(sparkPostResponse.get().getSparkPostResults().getTransmissionId());
+        transmission.setAudience_email(scheduledTransmissionRequest.getAddressList().get(0).getAddress());
+        transmission.setAudience(audienceRepository.getReferenceById(scheduledTransmissionRequest.getAudienceId()));
+        transmission.setUser(userRepository.getReferenceById(scheduledTransmissionRequest.getUserId()));
+        transmission.setCreatedAt(LocalDateTime.now());
+        transmission.setCreatedBy("" + scheduledTransmissionRequest.getUserId());
+        transmission.setJourney(journeyRepository.findById(scheduledTransmissionRequest.getJourneyId()).get());
+        transmissionRepository.save(transmission);
+
+        Response response = new Response();
+        response.setStatusCode(200);
+        response.setMsg("Scheduled Transmission successfully created");
+
+        //return task to core module
+        BaseTaskEntity coreModuleTask = new BaseTaskEntity();
+        restTemplate.postForObject("http://localhost:8081/ReturnTask", coreModuleTask, String.class);
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+}
+
---
+++ src/main/java/springredis/demo/entity/request/Address.java
@@ -0,0 +1,17 @@
+package springredis.demo.entity.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+    @JsonProperty("address")
+    private String address;
+}
