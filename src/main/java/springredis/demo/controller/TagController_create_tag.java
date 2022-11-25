fdfcbc51d922d6715993830f5750fe8dfff86067
james <jameswang962050@gmail.com>
2022-11-24 17:33:12 -0800

    tag

---
+++ src/main/java/springredis/demo/controller/TagController_create_tag.java
@@ -0,0 +1,20 @@
+package springredis.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import springredis.demo.Service.BackendTaskService;
+import springredis.demo.entity.CoreModuleTask;
+
+@RestController
+public class TagController_create_tag {
+
+    @Autowired
+    private BackendTaskService backendTaskService;
+
+    @PostMapping("/coreTask")
+    public CoreModuleTask createRelationBetweenAudienceAndTag(@RequestBody CoreModuleTask coreModuleTask) {
+        return backendTaskService.createRelationBetweenAudienceAndTag(coreModuleTask);
+    }
+}
