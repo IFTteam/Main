f23054b91952b7b7fe98c63355e404c7a70b2a2a
Kira Wang <ikirawang@gmail.com>
2022-12-04 22:42:57 -0500

    getmsys in event

--- src/main/java/springredis/demo/repository/TransmissionRepository.java
+++ src/main/java/springredis/demo/repository/TransmissionRepository.java
@@ -2,10 +2,21 @@ package springredis.demo.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
+import springredis.demo.entity.Journey;
 import springredis.demo.entity.Transmission;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface TransmissionRepository extends JpaRepository<Transmission, Long> {
+public interface TransmissionRepository extends JpaRepository<Transmission, Long>  {
+
+    @Query("select t.audience_email from Transmission t where t = ?1")
+    long getJourneyId(Transmission transmission);
+
+    @Query("select s from Transmission s where s.journey = ?1")
+    List<Transmission> getTransmissionByJourney(Optional<Journey> journey);
+
 }
