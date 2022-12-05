d3f2d19dd87b3f2a1659c3cd4f086ee084b61e31
Kira Wang <ikirawang@gmail.com>
2022-12-04 23:21:12 -0500

    getmsys in event22

--- src/main/java/springredis/demo/repository/AudienceRepository.java
+++ src/main/java/springredis/demo/repository/AudienceRepository.java
@@ -4,17 +4,27 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import springredis.demo.entity.Audience;
+import springredis.demo.entity.User;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AudienceRepository extends JpaRepository<Audience, Long> {
     //equivalent to typed query with entitiy menager
 
-@Query(value="SELECT a from Audience a WHERE a.id=:Id")                 //search audience by id in audience repo
-Audience searchAudienceByid(long Id);
+    @Query(value="SELECT a from Audience a WHERE a.id=:Id")                 //search audience by id in audience repo
+    Audience searchAudienceByid(long Id);
 
     @Query(value = "SELECT a from Audience a WHERE a.email=:Email")
     Audience searchAudienceByEmail(String Email);
+
+    @Query("select s.id from Audience s where s.user = ?1")
+    long getAudienceIdByUser(User user);
+
+    @Query("select s from Audience s where s.user = ?1")
+    List<Audience> getAudienceByUser(User user);
+
+    Audience findById(long l);
 
 }
