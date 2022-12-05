feb7c5994a454cf45d4c7c6e3549157a5d632d14
Kira Wang <ikirawang@gmail.com>
2022-12-04 21:28:49 -0500

    audience

--- src/main/java/springredis/demo/entity/Audience.java
+++ src/main/java/springredis/demo/entity/Audience.java
@@ -1,35 +1,78 @@
 package springredis.demo.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import springredis.demo.entity.activeEntity.ActiveNode;
 import springredis.demo.entity.base.BaseEntity;
 
 import javax.persistence.*;
-
-
 import java.sql.Date;
-import java.util.Set;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Audience extends BaseEntity {
     @Id
-    @GeneratedValue    
-    private Long id;
+    @SequenceGenerator(
+            name = "audience_sequence",
+            sequenceName = "audience_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "audience_sequence"
+    )
+    private long id;
     private String email;
     private String firstName;
     private String lastName;
     private String phone;
     private String address;
 
-    private Date birthday;
+    private LocalDate birthday;
     private String source;
 
     @ManyToOne(targetEntity = Node.class)
     @JoinColumn(name="audience_node_id",referencedColumnName = "id")
     private Node node;
-    
-    @OneToMany(mappedBy = "audience", fetch = FetchType.LAZY,
-            cascade = CascadeType.PERSIST, targetEntity = Transmission.class)
-    private Set<Transmission> transmissions;
+
+    @ManyToOne(
+            cascade = CascadeType.ALL
+    )
+    @JoinColumn(
+            name = "user_id"
+    )
+    private User user;
+
+    @ManyToMany(
+            fetch = FetchType.EAGER,
+            cascade = CascadeType.ALL
+    )
+    @JoinTable(
+            name = "map",
+            joinColumns = @JoinColumn(
+                    name = "audience_id"
+            ),
+            inverseJoinColumns = @JoinColumn(
+                    name = "tag_id"
+            )
+    )
+    @JsonIgnore
+    private List<Tag> tags = new ArrayList<>();
+
+    public void addTags(Tag tag){
+        if(tags == null) tags = new ArrayList<>();
+        tags.add(tag);
+    }
+    private LocalDate date_added;
+    private LocalDate last_updated_time;
 }
+
