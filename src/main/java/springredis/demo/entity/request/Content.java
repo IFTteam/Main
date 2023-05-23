37f8cb314bfd4caf170dda4c4b8dfb229a314ebe
hsw1026 <hhuang60@hawk.iit.edu>
2023-05-23 14:53:00 -0500

    Webhook update

--- src/main/java/springredis/demo/entity/request/Content.java
+++ src/main/java/springredis/demo/entity/request/Content.java
@@ -20,8 +20,25 @@ public class Content {
     private String subject;
     private String link;
 
+    private String link1; // Added variable for link1
+    private String link2; // Added variable for link2
+
     @JsonProperty("html")
     private String html;
+
+    public void setHtml(String link1, String link2) {
+        this.link1 = link1; // Set the value of link1
+        this.link2 = link2; // Set the value of link2
+        this.html = "<a href='" + link1 + "'>CNN news</a><br><a href='" + link2 + "'>Unsubscribe</a>";
+    }
+
+    public String getLink1() {
+        return link1;
+    }
+
+    public String getLink2() {
+        return link2;
+    }
 
     @JsonProperty("text")
     private String text;
