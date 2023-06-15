613aa3b999001f68239962a7f7a93f339127195f
shuangyangli <shuangyang_li@outlook.com>
2023-06-15 01:35:58 -0400

    update: getTransmission controller

--- src/main/java/springredis/demo/entity/Transmission.java
+++ src/main/java/springredis/demo/entity/Transmission.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Entity
 @Getter
