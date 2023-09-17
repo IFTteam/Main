a89434cf209f1bf17933b4eba996ad3bd846c2e6
ChenhaoXu66 <115743023+ChenhaoXu66@users.noreply.github.com>
2023-09-16 20:05:42 -0500

    Audience

--- src/main/java/springredis/demo/entity/base/BaseEntity.java
+++ src/main/java/springredis/demo/entity/base/BaseEntity.java
@@ -1,4 +1,6 @@
 package springredis.demo.entity.base;
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.format.DateTimeFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedBy;
@@ -6,6 +8,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import springredis.demo.utils.LocalDateTimeStringConverter;
 
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
@@ -16,21 +19,28 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
+
+    @DateTimeFormat("yyyy-MM-dd HH:mm:ss.SSSSSS")
+    @ExcelProperty(value = "created_at", converter = LocalDateTimeStringConverter.class)
     @CreatedDate
     @Column(updatable = false)
     @JsonIgnore
     private LocalDateTime createdAt;
 
+    @ExcelProperty("created_by")
     @CreatedBy
     @Column(updatable = false)
     @JsonIgnore
     private String createdBy;
 
+    @DateTimeFormat("yyyy-MM-dd HH:mm:ss.SSSSSS")
+    @ExcelProperty(value = "updated_at", converter = LocalDateTimeStringConverter.class)
     @LastModifiedDate
     @Column()
     @JsonIgnore
     private LocalDateTime updatedAt;
 
+    @ExcelProperty("updated_by")
     @LastModifiedBy
     @Column()
     @JsonIgnore
